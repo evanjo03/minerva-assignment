fe22511262998a57c157c5519768bfecb24c3cf6
John Evans <john.wes.evans@gmail.com>
2022-03-26 15:09:36 -0500

    Responsive attributes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,17 @@
-function onHamburgerClick() {
-  var x = document.getElementById("hamburger__menu");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "flex";
+const hamburgerButton = document.querySelector("#hamburger-btn");
+const hamburgerMenu = document.querySelector("#hamburger-menu");
+
+hamburgerButton.addEventListener("click", () => {
+  const display = hamburgerMenu.style.display;
+  hamburgerMenu.style.display = display === "flex" ? "none" : "flex";
+});
+
+/*Add the match function as a listener for state changes.  
+This is to ensure that if the hamburger menu is opened, then the window is resized 
+to a resolution where the hamburger menu is replaced with standard anchors, 
+the hamburger menu options will disappear. */
+window.matchMedia("(min-width: 450px)").addListener((mql) => {
+  if (mql.matches) {
+    hamburgerMenu.style.display = "none";
   }
-}
+});
