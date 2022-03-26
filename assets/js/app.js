54353ed34323bee4252ed7b4af86de7bc3a8f39e
John Evans <john.wes.evans@gmail.com>
2022-03-26 16:19:47 -0500

    Style touchup

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,31 +3,58 @@ const hamburgerMenu = document.querySelector("#hamburger-menu");
 const input = document.querySelector("#email-input");
 const button = document.querySelector("#submit-btn");
 
-input.addEventListener("input", () => {
-  /* Leveraging the built-in html email validation, which uses the following regex:
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-  */
-  button.disabled = !input.checkValidity();
-});
-
-button.addEventListener("click", () => {
-  if (!input.checkValidity()) return;
-
-  alert(`The email to submit is "${input.value}".`);
-  input.value = "";
-  button.disabled = true;
-});
-
-hamburgerButton.addEventListener("click", () => {
-  const display = hamburgerMenu.style.display;
-  hamburgerMenu.style.display = display === "flex" ? "none" : "flex";
-});
-
 /* This is to ensure that if the hamburger menu is opened and the window is resized 
 to a resolution where the hamburger menu is replaced with standard anchors, 
 the hamburger menu options will disappear. */
-window.matchMedia("(min-width: 768px)").addListener((mql) => {
+const mediaQueryList = window.matchMedia("(min-width: 768px)");
+
+function handleInput() {
+  /* Leveraging the built-in html email validation, which uses the following regex:
+    /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    */
+  button.disabled = !input.checkValidity();
+}
+
+function handleKeyDown(e) {
+  if (!(e.key === "Enter" || e.keyCode === 13) || !input.checkValidity())
+    return;
+  alert(`The email to submit is "${input.value}".`);
+  input.value = "";
+  button.disabled = true;
+}
+
+function handleSignUpButtonClick() {
+  if (!input.checkValidity()) return;
+}
+
+function resetForm() {
+  input.value = "";
+  button.disabled = true;
+}
+
+function submitForm() {
+  alert(`The email to submit is "${input.value}".`);
+
+  resetForm();
+}
+
+function handleHamburgerButtonClick() {
+  const display = hamburgerMenu.style.display;
+  hamburgerMenu.style.display = display === "flex" ? "none" : "flex";
+}
+
+function handleMediaQueryChange(mql) {
   if (mql.matches) {
     hamburgerMenu.style.display = "none";
   }
-});
+}
+
+function addEventListeners() {
+  input.addEventListener("input", () => handleInput());
+  input.addEventListener("keydown", (e) => handleKeyDown(e));
+  button.addEventListener("click", () => handleSignUpButtonClick());
+  hamburgerButton.addEventListener("click", () => handleHamburgerButtonClick());
+  mediaQueryList.addListener((mql) => handleMediaQueryChange(mql));
+}
+
+addEventListeners();
